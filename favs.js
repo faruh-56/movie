b48272b3e5e7266608c2bade09456bd9158e054f
faruh-26 <valievfarruh6@gmail.com>
2024-10-24 19:50:26 +0500

    update

--- favs.js
+++ favs.js
@@ -1,14 +1,12 @@
 let favMoviesList = JSON.parse(localStorage.getItem('favMovies')) || [];
 
-// Контейнер для отображения фильмов
+
 const favMoviesContainer = document.body.querySelector('.fav-movies-container');
 
-// Функция для обновления списка избранных фильмов в DOM и localStorage
+
 function renderFavMovies() {
-    // Очищаем контейнер перед рендерингом
     favMoviesContainer.innerHTML = '';
 
-    // Проходим по каждому фильму в списке
     favMoviesList.forEach((favMovie, index) => {
         const cardElementTemplate = `
         <div class="card" style="width: 20rem" data-card-id="${index}">
@@ -39,24 +37,21 @@ function renderFavMovies() {
             </div>
         </div>`;
 
-        // Добавляем шаблон карточки фильма в контейнер
+        
         favMoviesContainer.insertAdjacentHTML('beforeend', cardElementTemplate);
 
-        // Привязываем обработчик для удаления фильма
+        
         const removeFavMovieButton = favMoviesContainer.children[favMoviesContainer.children.length - 1].querySelector('.remove-button');
 
         removeFavMovieButton.addEventListener('click', () => {
-            // Удаляем фильм по индексу
             favMoviesList.splice(index, 1);
 
-            // Обновляем localStorage
             localStorage.setItem('favMovies', JSON.stringify(favMoviesList));
 
-            // Перерисовываем список фильмов
             renderFavMovies();
         });
     });
 }
 
-// Инициализация отображения фильмов
+
 renderFavMovies();
